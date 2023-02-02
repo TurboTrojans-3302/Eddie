18a0d55df7d61d5274d87df9d6de5d3abf0c9c5d
Ted Hansen <kc8nod@gmail.com>
2023-02-02 13:12:37 -0500

    set up the DriveTrain

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,19 +1,19 @@
 package frc.robot;
 
 public class RobotMap {
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2; // CAN
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 0; // Analog
-    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1; // CAN
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 0; 
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 1; 
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 2; 
 
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 4; // CAN
-    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 1; // Analog
-    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 3; // CAN
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 3; 
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 4; 
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 5; 
 
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 6; // CAN
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 2; // Analog
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 5; // CAN
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 6; 
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 7; 
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 8; 
 
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 8; // CAN
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 3; // Analog
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 7; // CAN
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 9; 
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 10; 
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 11; 
 }
