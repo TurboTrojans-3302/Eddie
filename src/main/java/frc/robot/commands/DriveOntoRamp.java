ec6b8280520b41067c4ed00bff496a972fe50960
Ted Hansen <kc8nod@gmail.com>
2023-02-18 09:07:25 -0500

    outline some new commands

---
+++ src/main/java/frc/robot/commands/DriveOntoRamp.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Drivetrain;
+
+public class DriveOntoRamp extends CommandBase {
+  private Drivetrain m_drivetrain;
+
+  /** Creates a new DriveOntoRamp. */
+  public DriveOntoRamp(Drivetrain subsystem) {
+    m_drivetrain = subsystem;
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    System.out.println("pitch is: " + m_drivetrain.getPitch());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
