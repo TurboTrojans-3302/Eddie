861531a4f4c36a53ed1a60095409ec2cf6224352
Ted Hansen <kc8nod@gmail.com>
2023-03-09 20:05:50 -0500

    first draft of Arm code

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -16,4 +16,18 @@ public class RobotMap {
     public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 6; 
     public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 11; 
     public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 5; 
+
+
+    public static final int SHOULDER_VALVE_FWD = 0;
+    public static final int SHOULDER_VALVE_BACK = 1;
+    public static final int EXTENSION_VALVE_OUT = 2;
+    public static final int EXTENSION_VALVE_IN = 3;
+    public static final int GRIPPER_VALVE_CLOSE = 4;
+    public static final int GRIPPER_VALVE_OPEN = 5;
+
+    public static final int ELBOW_MOTOR_ID = 13;
+    public static final int WRIST_MOTOR_ID = 14;
+
+
+
 }
